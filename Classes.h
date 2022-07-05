ba61c5e39551f3021be195f9ea69e0d274b3baf0
crrips <106340049+crrips@users.noreply.github.com>
2022-07-06 00:53:08 +0400

    Add files via upload

--- Classes.h
+++ Classes.h
@@ -1,7 +1,5 @@
 #pragma once
-#include <iostream>
 #include "Functions.h"
-using namespace std;
 
 class KeyPair {
 
@@ -14,20 +12,20 @@ public:
 
 	KeyPair genKeyPair(KeyPair &key) {
 				
-		genPrivateKey(key.privateKey);
-		genPublicKey(key.publicKey, key.privateKey);
+		genPrivateKey(privateKey);
+		genPublicKey(publicKey, privateKey);
 
 		return key;
 	}
 
-	void printDec(KeyPair key) {
-		cout << "Private key: " << key.privateKey << endl
-			<< "Public \tkey: " << key.publicKey << endl << endl;
+	void printDec() {
+		cout << "Private key: " << privateKey << endl
+			<< "Public \tkey: " << publicKey << endl << endl;
 	}
 
-	void printHex(KeyPair key) {
-		cout << "Private key: 0x"; little2hex(key.privateKey);
-		cout << "Public \tkey: 0x"; little2hex(key.publicKey);
+	void printHex() {
+		cout << "Private key: 0x"; dec2hex(privateKey);
+		cout << "Public \tkey: 0x"; dec2hex(publicKey);
 	}
 	
 	unsigned long long getPriv() {
@@ -42,14 +40,216 @@ public:
 
 class Signature {
 
+	int message;
+
 public:
 
-	void signData(unsigned long long key) {
-		sign(key);
+	int signData(int msg, unsigned long long key) {
+
+		//2 random prime numbers
+		double p = 11;
+		double q = 22;
+		double n = p * q;//calculate n
+		unsigned long long track;
+		double phi = (p - 1) * (q - 1);//calculate phi
+
+		double e = key;
+		//for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
+		while (e < phi) {
+			track = gcd(e, phi);
+			if (track == 1)
+				break;
+			else
+				e++;
+		}
+		unsigned long long c = pow(msg, e); //encrypt the message
+		c = fmod(c, n);
+
+		message = msg;
+
+		return c;
 	}
 
-	bool verifySignature() {
+	bool verifySignature(int sign, int msg, unsigned long long key) {
 
+		//2 random prime numbers
+		double p = 11;
+		double q = 22;
+		double n = p * q;//calculate n
+		unsigned long long track;
+		double phi = (p - 1) * (q - 1);//calculate phi
+
+		double e = key;
+		//for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
+		while (e < phi) {
+			track = gcd(e, phi);
+			if (track == 1)
+				break;
+			else
+				e++;
+		}
+
+		double d1 = 1 / e;
+		double d = fmod(d1, phi);
+		double c = pow(msg, e); //encrypt the message
+		unsigned long long m = pow(c, d);
+		m = fmod(m, n);
+
+		if (m == sign && msg == message)
+		{
+			//cout << "\nTrue" << endl;
+			return true;
+		}
+		else
+		{
+			//cout << "\nFalse" << endl;
+			return false;
+		}
+	}
+
+};
+
+class Account {
+
+private:
+
+	unsigned long long accountID;
+	unsigned long long wallet[100];
+	int balance = 0;
+	int index = 0;
+	int money = 0;
+
+public:
+
+	Account genAccount(Account &acc) {
+
+		KeyPair key;
+		key.genKeyPair(key);
+		//key.printDec();
+		key.printHex();
+
+		accountID = key.getPub();
+		wallet[index] = key.getPub();
+		wallet[index + 1] = key.getPriv();
+
+		index += 2;
+
+		return acc;
+	}
+
+	unsigned long long getPub(int index) {
+		return wallet[index];
+	}
+
+	void addKeyPairToWallet() {
+
+		KeyPair key;
+		key.genKeyPair(key);
+
+		wallet[index] = key.getPub();
+		wallet[index + 1] = key.getPriv();
+
+		index += 2;
+
+	}
+
+	void updateBalance(int &balance) {
+		
+		balance += money;
+		money = 0;
+
+	}
+
+	int getBalance() {
+		return balance;
+	}
+
+	void printBalance() {
+		cout << "\nBalance: " << balance << endl;
+	}
+
+	void createPaymentOp(Account receiver, int amount, int index) {
+	
+		balance -= amount;
+		receiver.money = amount;
+
+		updateBalance(receiver.balance);
+		
+	}
+
+	int signData(int msg, int index) {
+
+		Signature signature;
+		int sign = signature.signData(msg, wallet[index * 2 - 1]);
+		
+		return sign;
+	}
+
+};
+
+class Operation {
+
+private:
+
+	Account sender;
+	Account receiver;
+	int amount;
+	int sign;
+
+public:
+
+	Operation createOperation(Account sender, Account receiver, int amount, int signature) {
+	
+		Operation operation;
+
+		operation.sender = sender;
+		operation.receiver = receiver;
+		operation.amount = amount;
+		operation.sign = signature;
+
+		if (&Operation::verifyOperation) {
+			operation.sender.createPaymentOp(operation.receiver, operation.amount, 1);
+		}
+
+		return operation;
+	}
+
+	bool verifyOperation(Operation operation) {
+
+		Signature sign;
+
+		if (sign.verifySignature(operation.sign, 10, operation.sender.getPub(0))) {
+
+			if (operation.amount <= operation.sender.getBalance()) {
+
+				return true;
+
+			}
+
+		}
+
+	}
+
+};
+
+class Transaction {
+
+private:
+
+	string transactionID;
+	Operation setOfOperations[0xFFFFF];
+	int nonce = 0;
+
+public:
+
+	Transaction createTransaction(Operation operation) {
+
+		Transaction transaction;
+
+		transaction.setOfOperations[nonce] = operation;
+		transaction.nonce++;
+
+		return transaction;
 	}
 
 };
--- Functions.h
+++ Functions.h
@@ -3,9 +3,12 @@
 #include <string>
 #include <math.h>
 #include <random>
+#include <vector>
 using namespace std;
 
-void little2hex(unsigned long long dec) {
+//////////////////////////////////
+
+void dec2hex(unsigned long long dec) {
 
     string digits[16] = { "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F" };
     string hex;
@@ -58,9 +61,8 @@ void genPublicKey(unsigned long long &pubkey, unsigned long long privkey) {
 
 //////////////////////////////////
 
-// find gcd
-int gcd(unsigned long long a, unsigned long long b) {
-    unsigned long long t;
+int gcd(int a, int b) {
+    double t;
     while (1) {
         t = a % b;
         if (t == 0)
@@ -70,64 +72,4 @@ int gcd(unsigned long long a, unsigned long long b) {
     }
 }
 
-void sign(unsigned long long key) {
-
-    //2 random prime numbers
-    unsigned long long p = 11;
-    unsigned long long q = 22;
-    unsigned long long n = p * q;//calculate n
-    unsigned long long track;
-    double phi = (p - 1) * (q - 1);//calculate phi
-    //public key
-    //e stands for encrypt
-    double e = key;
-    //for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
-    while (e < phi) {
-        track = gcd(e, phi);
-        if (track == 1)
-            break;
-        else
-            e++;
-    }
-    unsigned long long message = 12;
-    unsigned long long c = pow(message, e); //encrypt the message
-    c = fmod(c, n);
-    cout << "\nOriginal Message = " << message;
-    cout << "\n" << "Encrypted message = " << c << endl;
-
-}
-
-/*
-//2 random prime numbers
-    unsigned long long p = 11;
-    unsigned long long q = 22;
-    unsigned long long n = p * q;//calculate n
-    unsigned long long track;
-    double phi = (p - 1) * (q - 1);//calculate phi
-    //public key
-    //e stands for encrypt
-    double e = key;
-    //for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
-    while (e < phi) {
-        track = gcd(e, phi);
-        if (track == 1)
-            break;
-        else
-            e++;
-    }
-    //private key
-    //d stands for decrypt
-    //choosing d such that it satisfies d*e = 1 mod phi
-    double d1 = 1 / e;
-    double d = fmod(d1, phi);
-    unsigned long long message = 12;
-    unsigned long long c = pow(message, e); //encrypt the message
-    double m = pow(c, d);
-    c = fmod(c, n);
-    m = fmod(m, n);
-    cout << "\nOriginal Message = " << message;
-    cout << "\n" << "e = " << e;
-    cout << "\n" << "d = " << d;
-    cout << "\n" << "Encrypted message = " << c;
-    cout << "\n" << "Decrypted message = " << m << endl;
-*/
+//////////////////////////////////
